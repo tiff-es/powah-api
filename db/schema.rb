afb3f4ae8284ab19431fce72548496eda7f19544
boostinwrx <tony.calablava@gmail.com>
2020-03-10 16:48:08 -0400

    Database migrated from sqlite to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_11_08_171214) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "cars", force: :cascade do |t|
     t.string "picture"
     t.string "make"
@@ -26,8 +29,8 @@ ActiveRecord::Schema.define(version: 2019_11_08_171214) do
   end
 
   create_table "user_cars", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "car_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "car_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["car_id"], name: "index_user_cars_on_car_id"
